6babc9b800606f707d6ca34451d3d6c8adc195d3
Adam J. Arling <adam.arling@northwestern.edu>
2021-02-12 08:47:54 -0600

    Update label for My Annotations

--- src/components/ShowHideAnnotations.js
+++ src/components/ShowHideAnnotations.js
@@ -1,14 +1,24 @@
 import React from 'react';
-import { Checkbox, Stack, Text } from '@chakra-ui/react';
+import { Box, Checkbox, Flex, Icon, Stack, Text } from '@chakra-ui/react';
+import { BiOutline } from 'react-icons/bi';
+
+const fontSize = 'sm';
 
 export default function ShowHideAnnotations() {
   return (
-    <Stack spacing={5} direction="row" ml={6}>
-      <Checkbox disabled={true}>
-        <Text fontSize="sm">Your Annotations</Text>
+    <Stack spacing={5} direction="row" ml={6} align="center">
+      <Flex alignItems="center">
+        <BiOutline />
+        <Text ml={2} fontFamily="ocrAStd">
+          Change Display:{' '}
+        </Text>
+      </Flex>
+
+      <Checkbox defaultIsChecked disabled={true} onChange={() => false}>
+        <Text fontSize={fontSize}>Your Annotations</Text>
       </Checkbox>
-      <Checkbox defaultIsChecked disabled={true}>
-        <Text fontSize="sm">Curators Annotations</Text>
+      <Checkbox disabled={true}>
+        <Text fontSize={fontSize}>Curators Annotations</Text>
       </Checkbox>
     </Stack>
   );
