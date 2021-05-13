756a8d3ca3719b779bc8993debbb8d61d6b26789
Adam J. Arling <aarling@gmail.com>
2021-05-13 09:15:48 -0500

    Issue 109 - update color mode light icon

--- src/ColorModeSwitcher.js
+++ src/ColorModeSwitcher.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import { useColorMode, useColorModeValue, IconButton } from '@chakra-ui/react';
-import { FaMoon, FaSun } from 'react-icons/fa';
+import { FaMoon } from 'react-icons/fa';
+import { ImSun } from 'react-icons/im';
 import useFabricHelpers from 'hooks/use-fabric-helpers';
 
 export const ColorModeSwitcher = props => {
   const { toggleColorMode } = useColorMode();
   const text = useColorModeValue('dark', 'light');
-  const SwitchIcon = useColorModeValue(FaMoon, FaSun);
+  const SwitchIcon = useColorModeValue(FaMoon, ImSun);
   const { updateControlStyle } = useFabricHelpers();
 
   const handleChangeColor = () => {
