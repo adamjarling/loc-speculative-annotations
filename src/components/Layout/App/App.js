23999a36c460e40050d5a9db8c6c5837c96c2c78
Adam J. Arling <aarling@gmail.com>
2021-05-30 11:02:00 -0500

    Issue #130 - Handle route transitions and save unsaved annotations

--- src/components/Layout/App/App.js
+++ src/components/Layout/App/App.js
@@ -9,7 +9,10 @@ import LayoutAppFooter from './Footer';
 import Div100vh from 'react-div-100vh';
 import ShareQueryParamHandler from 'components/Share/QueryParamHandler';
 import useKeyboardEvents from 'hooks/use-keyboard-events';
-import { useFabricOverlayState } from 'context/fabric-overlay-context';
+import {
+  useFabricOverlayDispatch,
+  useFabricOverlayState,
+} from 'context/fabric-overlay-context';
 import AdjustmentBar from 'components/AdjustmentBar/AdjustmentBar';
 import IntroMessage from 'components/IntroMessage';
 import LayoutAppMain from 'components/Layout/App/Main';
@@ -17,11 +20,16 @@ import { ToolbarOptionsProvider } from 'context/toolbar-options-context';
 
 function LayoutApp() {
   const { fabricOverlay, isToolbarVisible } = useFabricOverlayState();
+  const dispatch = useFabricOverlayDispatch();
+
+  // Initialize keyboard event handlers
   useKeyboardEvents();
 
   React.useEffect(() => {
     if (!fabricOverlay) return;
+    // Reset some default settings when coming into the App section
     fabricOverlay.fabricCanvas().hoverCursor = 'move';
+    dispatch({ type: 'updateTool', tool: 'POINTER' });
   }, [fabricOverlay]);
 
   return (
--- src/components/Layout/Site/Navbar.js
+++ src/components/Layout/Site/Navbar.js
@@ -1,24 +1,32 @@
+import React from 'react';
 import {
   Box,
+  Button,
   Flex,
   HStack,
   Link,
   IconButton,
-  Button,
   useDisclosure,
   useBreakpointValue,
   Stack,
 } from '@chakra-ui/react';
 import { CloseIcon } from '@chakra-ui/icons';
 import { BsThreeDots } from 'react-icons/bs';
-import { Link as RRLink, useHistory } from 'react-router-dom';
-import { useLocation, useParams } from 'react-router-dom';
+import {
+  Link as RRLink,
+  useHistory,
+  useLocation,
+  useParams,
+} from 'react-router-dom';
 import useButtonSize from 'hooks/use-button-size';
 import MyAnnotations from 'components/MyAnnotations/MyAnnotations';
 import { BiPencil } from 'react-icons/bi';
 import ClearCanvas from 'components/ClearCanvas';
 import UndoRedo from 'components/UndoRedo/UndoRedo';
 import Download from 'components/Download';
+import useFabricHelpers from 'hooks/use-fabric-helpers';
+import { useFabricOverlayState } from 'context/fabric-overlay-context';
+import ChangeWorkWarning from 'components/ChangeWorkWarning';
 
 export default function Simple() {
   const { isOpen, onOpen, onClose } = useDisclosure();
@@ -28,6 +36,11 @@ export default function Simple() {
   const buttonSize = useButtonSize();
   const location = useLocation();
   const isDesktop = useBreakpointValue({ base: false, lg: true });
+  const { getUserObjects } = useFabricHelpers();
+  const { activeUserCanvas, userCanvases } = useFabricOverlayState();
+  const [isChangeWorkWarningVisible, setIsChangeWorkWarningVisible] =
+    React.useState();
+  const [futureRoute, setFutureRoute] = React.useState();
 
   const activeStyles = {
     borderBottom: '2px solid',
@@ -43,8 +56,53 @@ export default function Simple() {
     return route === location.pathname;
   }
 
+  const handleNavClick = route => {
+    if (['/about', '/teach'].indexOf(location.pathname) > -1) {
+      return history.push(route);
+    }
+
+    // Save route where user may be heading
+    setFutureRoute(route);
+
+    const objects = getUserObjects();
+
+    // User is on an un-saved user canvas with no annotations
+    if (objects.length > 0 && !activeUserCanvas) {
+      setIsChangeWorkWarningVisible(true);
+      return;
+    }
+    // User is on a saved user canvas, with additional annotations
+    if (
+      activeUserCanvas &&
+      userCanvases[activeUserCanvas].fabricCanvas.objects.length !==
+        objects.length
+    ) {
+      setIsChangeWorkWarningVisible(true);
+      return;
+    }
+    history.push(route);
+  };
+
+  const handleChangeWorkWarningCancel = () => {
+    setIsChangeWorkWarningVisible(false);
+    // Go to route
+    history.push(futureRoute);
+    setFutureRoute(null);
+  };
+
+  const handleChangeWorkWarningSave = () => {
+    setIsChangeWorkWarningVisible(false);
+    document.getElementById('save-my-annotations').click();
+    setFutureRoute(null);
+  };
+
   return (
     <>
+      <ChangeWorkWarning
+        isVisible={isChangeWorkWarningVisible}
+        handleCancel={handleChangeWorkWarningCancel}
+        handleSave={handleChangeWorkWarningSave}
+      />
       <Box px={4}>
         <Flex
           h={16}
@@ -54,7 +112,21 @@ export default function Simple() {
         >
           <HStack spacing={8} alignItems={'center'}>
             <HStack as={'nav'} spacing={4} display="flex">
-              <Link
+              <Button
+                variant="link"
+                onClick={() => handleNavClick('/about')}
+                {...(isCurrentLink('/about') && { ...activeStyles })}
+              >
+                About
+              </Button>
+              <Button
+                variant="link"
+                onClick={() => handleNavClick('/teach')}
+                {...(isCurrentLink('/teach') && { ...activeStyles })}
+              >
+                Teach
+              </Button>
+              {/* <Link
                 as={RRLink}
                 to="/about"
                 {...(isCurrentLink('/about') && { ...activeStyles })}
@@ -67,7 +139,7 @@ export default function Simple() {
                 {...(isCurrentLink('/teach') && { ...activeStyles })}
               >
                 Teach
-              </Link>
+              </Link> */}
             </HStack>
           </HStack>
 
