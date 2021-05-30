ae50a4e2110e5ea2e34837e1ebd24ae2a531762e
Adam J. Arling <aarling@gmail.com>
2021-05-30 09:32:14 -0500

    Update intro message text

--- src/components/IntroMessage.js
+++ src/components/IntroMessage.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import {
   Button,
+  ListItem,
   Modal,
   ModalOverlay,
   ModalContent,
@@ -8,6 +9,7 @@ import {
   ModalFooter,
   ModalBody,
   ModalCloseButton,
+  OrderedList,
   Text,
   useDisclosure,
 } from '@chakra-ui/react';
@@ -26,17 +28,33 @@ export default function IntroMessage() {
       >
         <ModalOverlay />
         <ModalContent data-testid="intro-message">
-          <ModalHeader>Welcome to Speculative Annotation!</ModalHeader>
+          <ModalHeader>Welcome to Speculative Annotation</ModalHeader>
           <ModalCloseButton />
           <ModalBody>
-            <Lorem count={2} />
+            <Text>
+              If you could speak to history, what would you say? What questions
+              would you ask? What story would you tell?
+            </Text>
+            <Text>
+              Students can use this tool to examine pieces of history, learn
+              more about them, and interact with these items through annotation.
+              They can write a question, highlight a sentence, react with
+              stamps, or create a story.
+            </Text>
+            <Text>To get started:</Text>
+            <OrderedList mb={3}>
+              <ListItem>
+                <strong>Select</strong> an item from the Library of Congress
+                Speculative Annotation mini Collection.
+              </ListItem>
+              <ListItem>Annotate and learn!</ListItem>
+              <ListItem>Save and Share your annotation.</ListItem>
+            </OrderedList>
+            <Text>
+              Join this learning community and follow the conversation at
+              #annotateLOC.
+            </Text>
           </ModalBody>
-
-          <ModalFooter>
-            <Button mr={3} onClick={() => setIsModalOpen(false)}>
-              Agree
-            </Button>
-          </ModalFooter>
         </ModalContent>
       </Modal>
     </>
